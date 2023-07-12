d54ae3cbfc500a69a0034ef160835e228e12eefd
mego <megocapps@gmail.com>
2023-07-11 23:44:52 -0400

    add commas
    Signed-off-by: mego <megocapps@gmail.com>

--- react-1/addCommas.js
+++ react-1/addCommas.js
@@ -1,3 +1,22 @@
 function addCommas() {}
 
+function addCommas(number) {
+    const numberString = number.toString(); // Convert the number to a string
+    let result = '';
+    let count = 0;
+  
+    for (let i = numberString.length - 1; i >= 0; i--) {
+      result = numberString[i] + result;
+      count++;
+  
+      if (count % 3 === 0 && i !== 0) {
+        result = ',' + result;
+      }
+    }
+  
+    return result;
+  }
+ 
+
+
 module.exports = addCommas;
--- react-1/addCommas.test.js
+++ react-1/addCommas.test.js
@@ -4,4 +4,12 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+console.log(addCommas(1234)); // Expected output: "1,234"
+console.log(addCommas(1000000)); // Expected output: "1,000,000"
+console.log(addCommas(9876543210)); // Expected output: "9,876,543,210"
+console.log(addCommas(6)); // Expected output: "6"
+console.log(addCommas(-10)); // Expected output: "-10"
+console.log(addCommas(-5678)); // Expected output: "-5,678"
+
 });
